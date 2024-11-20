50731b04cbeea206b179f0931579a44ae4cf0525
HP <hp@hp.com>
2024-11-21 05:24:53 +0530

    added priorities

--- src/test/java/TestNGDemos/FirstTestCase.java
+++ src/test/java/TestNGDemos/FirstTestCase.java
@@ -9,17 +9,17 @@ import org.testng.annotations.Test;
 
 public class FirstTestCase {
 
-    @Test
+    @Test(priority = 1)
     void openapp () {
         System.out.println("app opened");
     }
 
-    @Test
+    @Test(priority = 2)
     void login () {
         System.out.println("logged in");
     }
 
-    @Test
+    @Test(priority = 3)
     void logout () {
         System.out.println("logged out");
     }
